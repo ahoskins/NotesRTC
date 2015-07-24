08f9c3e561654c5c17d7a5a684e4e36816550427
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-07-23 20:51:31 -0600

    features/share: wrap google api calls in promises with Q

--- src/client/controller.jsx
+++ src/client/controller.jsx
@@ -3,5 +3,24 @@ var Google = require('./google.js');
 
 React.render(<Search />, document.getElementById('content'));
 
+function processContacts(raw) {
+	console.dir('third');
+	var response = raw.currentTarget.response;
+	var parser = new DOMParser();
+	var xmlDoc = parser.parseFromString(response,"text/xml");
+	var children = xmlDoc.documentElement.children;
+	for (var i = 0; i < children.length; i++) {
+		if (children[i].nodeName !== 'entry') continue;
+		var grandchildren = children[i].children;
+		for (var j = 0; j < grandchildren.length; j++) {
+			if (grandchildren[j].nodeName !== 'gd:email') continue;
+			emails.push(grandchildren[j].attributes.getNamedItem('address').nodeValue);
+		}
+	}
+	console.dir(emails);
+}
+
+var emails = [];
 var g = Google();
-g.getContacts();
+g.getToken().then(g.getContacts).then(processContacts);
+
--- src/client/utils.js
+++ src/client/utils.js
@@ -14,7 +14,6 @@ module.exports = {
 				}
 			}
 		}
-		console.dir(result);
 		return result;	
 	}
 }
